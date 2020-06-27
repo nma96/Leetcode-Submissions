2b5f5c72b04ca59b55c16ea1e0a916cd9bfa3aed
Nikhil Mohan Aatrei <60794041+nma96@users.noreply.github.com>
2020-06-27 16:46:43 -0400

    Path Sum II 1591651662

---
+++ Path Sum II.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        if not root:
+            return []
+        res = []
+        self.dfs(root, sum, [], res)
+        return res
+
+    def dfs(self, root, sum, ls, res):
+        if not root.left and not root.right and sum == root.val:
+            ls.append(root.val)
+            res.append(ls)
+        if root.left:
+            self.dfs(root.left, sum-root.val, ls+[root.val], res)
+        if root.right:
+            self.dfs(root.right, sum-root.val, ls+[root.val], res)
