080a57a9912c9db5e73a1a83ce7c2887e1cb55cb
Nikhil Mohan Aatrei <60794041+nma96@users.noreply.github.com>
2020-06-27 19:16:10 -0400

    3Sum Closest 1593299585

---
+++ 3Sum Closest.py
@@ -0,0 +1,22 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        result = sum(nums[:3])
+        
+        for i in range(len(nums)-2):
+            left, right = i + 1, len(nums)-1
+            
+            while left < right:
+                currSum = nums[i] + nums[left] + nums[right]
+                
+                # check if new sum is smaller than previous
+                # if it is, update result
+                if abs(currSum - target) < abs(result - target):
+                    result = currSum
+                
+                # To find the other number sums, iterate through
+                if currSum < target:
+                    left += 1
+                else:
+                    right -= 1
+        return result
