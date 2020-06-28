85f01bd5fa47ddb3121e9b57ee43abaaa6dc9f3e
Nikhil Mohan Aatrei <60794041+nma96@users.noreply.github.com>
2020-06-27 20:10:25 -0400

    Word Break 1593302968

---
+++ Word Break.py
@@ -0,0 +1,10 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1) # dp[i] means s[:i+1] can be segmented into words in the wordDicts 
+        dp[0] = True
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if dp[i] and s[i: j+1] in wordDict:
+                    dp[j+1] = True
+        print(dp)      
+        return dp[-1]
