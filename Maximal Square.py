3e3a05d8db72dd956322cd30ed62b9461327ab41
Nikhil Mohan Aatrei <60794041+nma96@users.noreply.github.com>
2020-06-27 20:32:01 -0400

    Maximal Square 1593304207

---
+++ Maximal Square.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        # https://www.youtube.com/watch?v=rMw2cnpmP-4
+        m = len(matrix)
+        if m == 0: return 0
+        n = len(matrix[0])
+        height = 0
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if matrix[i-1][j-1]=='1':
+                    dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
+                    height = max(height, dp[i][j])
+        return height*height
+                    
