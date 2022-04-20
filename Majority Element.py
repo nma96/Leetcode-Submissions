314f46cfe2af82e8fbff8639ca51fd3f7b456222
Nikhil Mohan Aatrei <60794041+nma96@users.noreply.github.com>
2022-04-19 21:00:49 -0400

    Majority Element 1650416436

--- Majority Element.py
+++ Majority Element.py
@@ -1,14 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dict = collections.OrderedDict()
-        n = len(nums)//2
-        
+        count = {}
+        reqCount = len(nums)//2
         for i in range(len(nums)):
-            if nums[i] in dict:
-                dict[nums[i]] += 1
-            else:
-                dict[nums[i]] = 1
-        
-        for i in range(len(nums)):
-            if dict[nums[i]] > n:
-                return nums[i]
+            if nums[i] in count: 
+                count[nums[i]] += 1
+                if count[nums[i]] > reqCount: 
+                    return nums[i]
+            else: 
+                count[nums[i]] = 1
+                if count[nums[i]] > reqCount: 
+                    return nums[i]
