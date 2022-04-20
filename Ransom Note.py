82d6f04ef9afeb92686be25119c62c0edf2cceec
Nikhil Mohan Aatrei <60794041+nma96@users.noreply.github.com>
2022-04-19 21:00:50 -0400

    Ransom Note 1650416415

--- Ransom Note.py
+++ Ransom Note.py
@@ -1,19 +1,20 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mag = {}
-
-        for i in range(len(magazine)):
-            if magazine[i] not in mag:
-                mag[magazine[i]] = 1
-            else:
-                mag[magazine[i]] += 1
+        possibleLetters = {}
+        
+        for i in range(len(magazine)): 
+            if magazine[i] in possibleLetters: 
+                possibleLetters[magazine[i]] += 1
+            else: 
+                possibleLetters[magazine[i]] = 1
         
         for i in range(len(ransomNote)):
-            if ransomNote[i] in mag:
-                mag[ransomNote[i]] -= 1
-                if mag[ransomNote[i]] <= 0:
-                    del mag[ransomNote[i]]
-            else:
+            if ransomNote[i] not in possibleLetters.keys(): 
                 return False
+            else: 
+                possibleLetters[ransomNote[i]] -= 1
+                
+            if possibleLetters[ransomNote[i]] == 0: 
+                del possibleLetters[ransomNote[i]]
         
         return True
